947370fe0e9fd6255546854775dac3475b473593
Craig Doremus <cdoremus@gmail.com>
2023-11-04 23:20:04 -0400

    Moved both web and ws serving to main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,35 @@
 Deno.serve({
-  port: 8000,
-  handler: async (request) => {
-    // If the request is a normal HTTP request,
-    // we serve the client HTML file.
-    const file = await Deno.open("./index.html", { read: true });
-    return new Response(file.readable);
+  port: Deno.env.get("IS_PROD") ? 80 : 8080,
+  handler: async (request: Request) => {
+    const { pathname } = new URL(request.url);
+    // If the request is a websocket upgrade,
+    if (pathname.includes("/ws")) {
+      // we need to use the Deno.upgradeWebSocket helper
+      if (request.headers.get("upgrade") === "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket(request);
+
+        socket.onopen = () => {
+          console.log("CONNECTED");
+        };
+        socket.onmessage = (event) => {
+          console.log(`RECEIVED: ${event.data}`);
+          socket.send("pong");
+        };
+        socket.onclose = () => console.log("DISCONNECTED");
+        socket.onerror = (error) => console.error("ERROR:", error);
+
+        return response;
+      } else {
+        // If the request is a normal HTTP request,
+        // we serve the client HTML file.
+        // const file = await Deno.open("./index.html", { read: true });
+        return new Response("Websocket is not working");
+      }
+    } else {
+      // If the request is a normal HTTP request,
+      // we serve the client HTML file.
+      const file = await Deno.open("./index.html", { read: true });
+      return new Response(file.readable);
+    }
   },
 });
